0838629a17f0b273b41d3d3de3b45186e54bd25f
Decrapifier <decrapifier@govspend.com>
2024-11-04 19:38:28 +0000

    Automagically formatted by Duti!
    https://github.com/smartprocure/duti

--- src/index.test.js
+++ src/index.test.js
@@ -430,7 +430,7 @@ describe('submit()', () => {
     const submit = async () => {
       throw new ValidationError('My submit failed', {
         'location.addresses.0.tenants.0': ['invalid format'],
-        '': ['top level error']
+        '': ['top level error'],
       })
     }
     form = Form({ fields: goodFields, value: goodValue, submit })
@@ -439,11 +439,11 @@ describe('submit()', () => {
     expect(form.submit.state.error.message).toBe('My submit failed')
     expect(form.submit.state.error.cause).toEqual({
       'location.addresses.0.tenants.0': ['invalid format'],
-      '': ['top level error']
+      '': ['top level error'],
     })
     expect(form.errors).toEqual({
       'location.addresses.0.tenants.0': ['invalid format'],
-      '': ['top level error']
+      '': ['top level error'],
     })
     expect(form.submitError).toBe('My submit failed')
     expect(result).toBeUndefined()
